3f96f80f007edcb23afc38ddbfbf9c29097c4ba5
mayourt <mayourt@foxmail.com>
2019-08-06 23:41:44 +0800

    repair error commit

--- init.el
+++ init.el
@@ -3,113 +3,97 @@
 ;;; Code:
 
 (package-initialize)
-;; (setq-default initial-major-mode 'fundamental-mode)
-
-(setq mac-option-modifier  'meta)
-(setq mac-command-modifier 'hyper)
-(global-set-key [(hyper q)] 'save-buffers-kill-terminal)
+(setq-default package-archives
+      '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+        ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/snails"))
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode 0)
-
 (column-number-mode t)
 (line-number-mode t)
+(set-frame-font "Fira Code-20")
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message nil)
+(setq initial-major-mode 'text-mode)
+(load-theme 'eziam-light t)
+(set-background-color "#c9cfcf")
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+
 (show-paren-mode t)
 (global-auto-revert-mode t)
 (global-hl-line-mode t)
-
 (fset 'yes-or-no-p'y-or-n-p)
-
-(set-frame-font "Fira Code-20")
-(load-theme 'sanityinc-solarized-light t)
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
 (setq split-height-threshold nil)
 (setq split-width-threshold 0)
 (setq auto-save-default nil)
 (setq scroll-margin 3
       scroll-preserve-screen-position t)
 
-(setq-default python-shell-interpreter "python"
-              indent-tabs-mode nil
+(setq-default indent-tabs-mode nil
               c-basic-offset 4
               c-default-style "linux"
               default-tab-width 4)
 
-(setq-default package-archives
-      '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-        ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(require 'exec-path-from-shell)
+(exec-path-from-shell-initialize)
 
-(eval-when-compile
-  (require 'use-package))
-(setq use-package-verbose t)
+(require 'ivy)
+(setq ivy-initial-inputs-alist nil)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "")
+(bind-key* "M-x"     'counsel-M-x)
+(bind-key* "C-x b"   'counsel-switch-buffer)
+(bind-key* "C-x C-f" 'counsel-find-file)
+(bind-key* "C-c c"   'counsel-compile)
+(bind-key* "C-s"     'swiper)
+(bind-key* "C-c C-r" 'ivy-resume)
 
-(use-package subword
-  :diminish superword-mode
-  :config (global-superword-mode t))
+(require 'avy)
+(bind-key* "M-c" 'avy-goto-char)
 
-(use-package counsel
-  :bind (("M-x"     . counsel-M-x)
-         ("C-x c f" . counsel-find-file)
-         ("C-x b"   . counsel-switch-buffer)))
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 20)
+(setq recentf-max-saved-items 20)
 
-(use-package swiper
-  :bind ("C-s" . swiper))
+(require 'undo-tree)
+(add-hook 'after-init-hook 'global-undo-tree-mode)
+(setq undo-tree-visualizer-timestamps t)
+(setq undo-tree-visualizer-diff nil)
 
-(use-package ivy
-  :config
-  (setq ivy-initial-inputs-alist nil))
+(require 'company)
+(setq company-show-numbers t)
+(setq company-dabbrev-downcase nil)
+(setq company-idle-delay 0.2)
+(setq company-minimum-prefix-length 2)
+(setq company-tooltip-limit 20)
 
-(use-package avy
-  :bind (("M-c" . avy-goto-char)
-         ("M-s" . avy-goto-word-1)))
+(require 'company-tabnine)
+(add-to-list 'company-backends #'company-tabnine)
 
-(use-package eglot
-  :hook ((rust-mode . eglot-ensure)
-         (python-mode . eglot-ensure)
-         (c-mode . eglot-ensure)))
+(require 'diminish)
+(eval-after-load 'undo-tree
+  '(diminish 'undo-tree-mode))
+(eval-after-load 'company
+  '(diminish 'company-mode))
+(eval-after-load 'yasnippet
+  '(diminish 'yas-minor-mode))
+(eval-after-load 'hungry-delete
+  '(diminish 'hungry-delete-mode))
+(eval-after-load 'subword
+  '(diminish 'subword-mode))
+(diminish 'eldoc-mode)
 
-(use-package flymake
-  :hook (prog-mode . flymake-mode))
+;; common settings for prog mode
+(add-hook 'prog-mode-hook 'company-mode)
+(add-hook 'prog-mode-hook 'yas-minor-mode)
+(add-hook 'prog-mode-hook 'subword-mode)
+(add-hook 'prog-mode-hook 'hungry-delete-mode)
 
-(use-package company
-  :hook (prog-mode . company-mode)
-  :diminish company-mode
-  :config
-  (setq company-idle-delay 0.2
-        company-tooltip-limit 10
-        company-minimum-prefix-length 2
-        company-tooltip-align-annotations t
-        company-require-match 'never))
+;; some setting for rust-mode
 
-(use-package undo-tree
-  :defer 1
-  :diminish undo-tree-mode
-  :config
-  (progn
-    (global-undo-tree-mode)
-    (setq undo-tree-visualizer-timestamps t
-          undo-tree-visualizer-diff nil)))
-
-(use-package rust-mode
-  :mode "\\.rs\\'"
-  :init
-  (setq rust-format-on-save t)
-  :bind
-  ("C-c C-k" . rust-run))
-
-(use-package recentf
-  :config
-  (recentf-mode 1))
-
-(use-package yasnippet
-  :config
-  (yas-global-mode))
-
-(use-package hungry-delete
-  :config
-  (global-hungry-delete-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -118,7 +102,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (eglot hungry-delete yasnippet-snippets color-theme-sanityinc-solarized wgrep smex counsel avy sudo-edit use-package diminish rust-mode undo-tree company))))
+    (symbol-overlay yasnippet-snippets undo-tree smex rust-mode magit hungry-delete eziam-theme exec-path-from-shell diminish counsel company-tabnine bind-key avy))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -126,6 +110,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
+ 
 (provide 'init)
 ;;; init.el ends here
